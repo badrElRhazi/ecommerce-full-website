2f0bc2174fccb46573ce29a9e0507fb2fce183d3
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-17 00:08:48 +0200

    More product's details controller

--- app/Controllers/shorts.php
+++ app/Controllers/shorts.php
@@ -6,24 +6,29 @@ class shorts extends ClassProduct
         $product = new ClassProduct();
         $products  = $product ->findAll();
 
-        // Passer les données des utilisateurs à la vue
-        $short=$product->findShort(); // Find Sport products..
+        // Passer les données des utilisateurs à la vue (Fetch user's data to the view)
+        $short=$product->findShort(); // Find Sport products.. 
         $data = ['products' => $products,
                   'short'=> $short ];
         
-        // Afficher la vue
+        // Afficher la vue (show view)
         $this->view('page_Off','short', $data);
     }
-    public function shortdetails($id) {
+    // (More product's details)
+    public function shortydetails() {
         $product = new ClassProduct();
         if ($_SERVER['REQUEST_METHOD'] == 'GET'){
             $id = $_GET['id'];
-        $details  = $product ->findetails($id);
+            if($id===null){
+                exit;
+            }else{
+                 $details  = $product ->findetails($id);
+            }
+        
 
-        // Passer les données des utilisateurs à la vue
+        // Passer les données des utilisateurs à la vue 
         $short=$product->findShort(); // Find Sport products..
-        $data = ['details' => $details,
-                  'short'=> $short ];
+        $data = ['details' => $details ];
         
         // Afficher la vue
         $this->view('page_Off','product-details', $data);
