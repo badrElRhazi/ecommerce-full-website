0e0471cf31164b416faca81264067fec1787e127
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-17 00:12:13 +0200

    Product details improvements

--- app/Views/page_Off/product-details.php
+++ app/Views/page_Off/product-details.php
@@ -10,17 +10,6 @@
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="canonical" href="Replace_with_your_PAGE_URL" />
 
-    <!-- Open Graph (OG) meta tags are snippets of code that control how URLs are displayed when shared on social media  -->
-    <!-- <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Uthr – Fashion eCommerce HTML Template" />
-    <meta property="og:url" content="PAGE_URL" />
-    <meta property="og:site_name" content="SITE_NAME" /> -->
-    <!-- For the og:image content, replace the # with a link of an image -->
-    <!-- <meta property="og:image" content="#" />
-    <meta property="og:description"
-        content="240+ Best Bootstrap Templates are available on this website. Find your template for your project compatible with the most popular HTML library in the world." /> -->
-
     <!-- Add site Favicon -->
     <link rel="icon" href="https://hasthemes.com/wp-content/uploads/2019/04/cropped-favicon-32x32.png" sizes="32x32" />
     <link rel="icon" href="https://hasthemes.com/wp-content/uploads/2019/04/cropped-favicon-192x192.png"
