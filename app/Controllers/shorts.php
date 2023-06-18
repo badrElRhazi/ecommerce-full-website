c4eaade0cd26fb75dc0d6ca1d51105992c1db240
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-18 12:20:43 +0200

    link shorts with panel

--- app/Controllers/shorts.php
+++ app/Controllers/shorts.php
@@ -1,39 +1,55 @@
-<?php 
+<?php
 class shorts extends ClassProduct
-{ 
+{
     use Controller;
-    public function index() {
+
+    public function index()
+    {
         $product = new ClassProduct();
-        $products  = $product ->findAll();
+        $products = $product->findAll();
 
         // Passer les données des utilisateurs à la vue (Fetch user's data to the view)
-        $short=$product->findShort(); // Find Sport products.. 
-        $data = ['products' => $products,
-                  'short'=> $short ];
-        
+        $short = $product->findShort(); // Find Sport products..
+        $data = [
+            'products' => $products,
+            'short' => $short
+        ];
+
         // Afficher la vue (show view)
-        $this->view('page_Off','short', $data);
+        $this->view('page_Off', 'short', $data);
     }
+
     // (More product's details)
-    public function shortydetails() {
-        $product = new ClassProduct();
-        if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-            $id = $_GET['id'];
-            if($id===null){
-                exit;
-            }else{
-                 $details  = $product ->findetails($id);
-            }
-        
+    public function shortydetails()
+{
+    $product = new ClassProduct();
+    if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])) {
+        $id = $_GET['id'];
 
-        // Passer les données des utilisateurs à la vue 
-        $short=$product->findShort(); // Find Sport products..
-        $data = ['details' => $details ];
-        
-        // Afficher la vue
-        $this->view('page_Off','product-details', $data);
+        // Check if the ID is valid
+        if ($id === null) {
+            // Handle the invalid ID case (e.g., redirect or show an error message)
+            exit;
+        } else {
+            $details = $product->findetails($id);
         }
-    }
-    }
 
+        // Check if the Add to Wishlist button is clicked
+        if (isset($_POST['add_to_wishlist'])) {
+            // Get the product ID from the POST data
+            $productId = $_POST['product_id'];
+        }
+
+        // Pass the product details to the view
+        $data = [
+            'details' => $details
+        ];
+
+        // Show the view
+        $this->view('page_Off', 'product-details', $data);
+    } else {
+        exit;
+    }
+}
+}
 ?>
